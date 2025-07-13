529aa6b4b15862b4cce163f92051ffbe8d7641a6
SebasParra <sebasraster10@gmail.com>
2025-07-13 18:28:34 -0500

    Cambios finales

--- src/app/fotos/fotos.component.ts
+++ src/app/fotos/fotos.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { SupabaseService } from '../services/supabase.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-fotos',
-  imports: [NgFor],
+  imports: [NgFor,NgIf],
   templateUrl: './fotos.component.html',
   styleUrl: './fotos.component.scss'
 })
 export class FotosComponent implements OnInit{
   imagenes: any[] = [];
+  imagenSeleccionada: any = null;
 
   constructor(private supabaseService: SupabaseService) {}
 
@@ -19,6 +20,14 @@ export class FotosComponent implements OnInit{
   console.log('Datos recibidos desde Supabase:', datos);
   this.imagenes = datos;
 }
+
+  abrirModal(imagen : any) {
+    this.imagenSeleccionada = imagen; // Reiniciar la imagen seleccionada al abrir el modal
+  }
+
+  cerrarModal() {
+    this.imagenSeleccionada = null; // Reiniciar la imagen seleccionada al cerrar el modal
+  }
 
 
 }
